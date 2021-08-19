e6e43f41e93275599cbaffcc70b2c25f27efb41e
Adrian Witaszak <adrianwitaszak.gmail.com>
2021-08-19 18:46:40 +0100

    added new Dao's

--- app/src/main/java/com/adwi/pexwallpapers/data/local/WallpaperDatabase.kt
+++ app/src/main/java/com/adwi/pexwallpapers/data/local/WallpaperDatabase.kt
@@ -2,23 +2,25 @@ package com.adwi.pexwallpapers.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.adwi.pexwallpapers.data.local.entity.CuratedWallpapers
-import com.adwi.pexwallpapers.data.local.entity.SearchQueryRemoteKey
-import com.adwi.pexwallpapers.data.local.entity.SearchResult
-import com.adwi.pexwallpapers.data.local.entity.Wallpaper
+import com.adwi.pexwallpapers.data.local.dao.*
+import com.adwi.pexwallpapers.data.local.entity.*
 
 @Database(
     entities = [
         Wallpaper::class,
         CuratedWallpapers::class,
         SearchResult::class,
-        SearchQueryRemoteKey::class
+        SearchQueryRemoteKey::class,
+        Suggestion::class
     ],
-    version = 2
+    version = 1
 )
 abstract class WallpaperDatabase : RoomDatabase() {
 
-    abstract fun wallpaperDao(): WallpapersDao
-
+    abstract fun curatedDao(): CuratedDao
+    abstract fun favoritesDao(): FavoritesDao
+    abstract fun searchDao(): SearchDao
     abstract fun searchQueryRemoteKeyDao(): SearchQueryRemoteKeyDao
+    abstract fun suggestionsDao(): SuggestionsDao
+    abstract fun wallpaperDao(): WallpapersDao
 }

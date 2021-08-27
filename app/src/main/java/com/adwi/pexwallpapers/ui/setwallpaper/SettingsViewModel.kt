2f3e4419525bb94fb325f16da5493597825d1e0c
Adrian Witaszak <adrianwitaszak.gmail.com>
2021-08-27 09:57:14 +0100

    created SettingsViewModel.kt

---
+++ app/src/main/java/com/adwi/pexwallpapers/ui/setwallpaper/SettingsViewModel.kt
@@ -0,0 +1,6 @@
+package com.adwi.pexwallpapers.ui.setwallpaper
+
+import com.adwi.pexwallpapers.data.WallpaperRepository
+import com.adwi.pexwallpapers.shared.base.BaseViewModel
+
+class SettingsViewModel(private val repository: WallpaperRepository) : BaseViewModel()

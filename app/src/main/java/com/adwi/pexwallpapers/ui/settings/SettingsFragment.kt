8b114b8abd385088f45b45fb01e760753321d3de
Adrian Witaszak <adrianwitaszak.gmail.com>
2021-08-27 09:53:50 +0100

    created SettingsFragment

--- app/src/main/java/com/adwi/pexwallpapers/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/adwi/pexwallpapers/ui/settings/SettingsFragment.kt
@@ -1,12 +1,36 @@
 package com.adwi.pexwallpapers.ui.settings
 
-import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
-import com.adwi.pexwallpapers.R
+import android.view.MenuItem
+import androidx.fragment.app.viewModels
+import com.adwi.pexwallpapers.databinding.FragmentSettingsBinding
+import com.adwi.pexwallpapers.shared.base.BaseFragment
+import com.adwi.pexwallpapers.ui.wallpapers.WallpaperViewModel
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : BaseFragment<FragmentSettingsBinding, Any>(
+    inflate = FragmentSettingsBinding::inflate
+) {
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    override val viewModel: WallpaperViewModel by viewModels()
+
+    override fun setupToolbar() {
+
+    }
+
+
+    override fun setupViews() {
+
+    }
+
+    override fun setupFlows() {
+
+    }
+
+    override fun setupListeners() {
+
+    }
+
+    override fun setupAdapters() {}
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return false
     }
 }

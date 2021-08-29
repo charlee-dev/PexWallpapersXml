926f297ce3e55d627c7335e1bf2f50ace4acaca7
Adrian Witaszak <adrianwitaszak.gmail.com>
2021-08-29 12:15:23 +0100

    created

---
+++ app/src/test/java/com/adwi/pexwallpapers/ui/wallpapers/WallpaperViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.adwi.pexwallpapers.ui.wallpapers
+
+import org.junit.After
+import org.junit.Before
+
+class WallpaperViewModelTest {
+
+    private lateinit var viewModel: WallpaperViewModel
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}

76bbd034f893b46f22209f2505158d2c49dc0e4b
Adrian Witaszak <adrianwitaszak.gmail.com>
2021-09-06 18:08:26 +0100

    added channel groups

--- app/src/main/java/com/adwi/pexwallpapers/shared/tools/NotificationTools.kt
+++ app/src/main/java/com/adwi/pexwallpapers/shared/tools/NotificationTools.kt
@@ -1,6 +1,7 @@
 package com.adwi.pexwallpapers.shared.tools
 
 import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
@@ -20,78 +21,122 @@ enum class Channel {
 
 class NotificationTools @Inject constructor(
     @ApplicationContext private val context: Context,
-    private val sharingTools: SharingTools
+    private val imageTools: ImageTools
 ) {
-
     private lateinit var CHANNEL_ID: String
 
+    private val wallpaperGroupId = "wallpaper_group"
+    private val appGroupId = "app_group"
 
-    fun createNotificationChannel(channel: Channel) {
+    fun setupNotifications() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val wallpaperGroupName = context.getString(R.string.wallpapers)
+            val appGroupName = context.getString(R.string.other)
+
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannelGroup(
+                NotificationChannelGroup(
+                    wallpaperGroupId,
+                    wallpaperGroupName
+                )
+            )
+            notificationManager.createNotificationChannelGroup(
+                NotificationChannelGroup(
+                    appGroupId,
+                    appGroupName
+                )
+            )
+            createNotificationChannel(Channel.NEW_WALLPAPER)
+            createNotificationChannel(Channel.RECOMMENDATIONS)
+            createNotificationChannel(Channel.INFO)
+        }
+    }
+
+
+    private fun createNotificationChannel(channel: Channel) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             var name = ""
             var importance = 0
+            val channelGroup: String
             when (channel) {
                 Channel.NEW_WALLPAPER -> {
                     CHANNEL_ID = "pex_new_wallpaper"
                     name = "New wallpaper"
                     importance = NotificationManager.IMPORTANCE_DEFAULT
+                    channelGroup = wallpaperGroupId
                 }
                 Channel.RECOMMENDATIONS -> {
                     CHANNEL_ID = "pex_recommendations"
                     name = "Recommendations"
                     importance = NotificationManager.IMPORTANCE_DEFAULT
+                    channelGroup = wallpaperGroupId
                 }
                 Channel.INFO -> {
                     CHANNEL_ID = "info"
                     name = "Info"
                     importance = NotificationManager.IMPORTANCE_HIGH
+                    channelGroup = appGroupId
                 }
             }
             val notificationChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            notificationChannel.group = channelGroup
             val notificationManager: NotificationManager =
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(notificationChannel)
         }
     }
 
-    suspend fun sendNotification(channel: Channel, imageUrl: String) {
+    suspend fun sendNotification(channel: Channel, imageUrl: String, longMessage: String) {
         var notificationNewWallpaperId = 101
         var requestCode = 1
+        val intentDestination: Class<*>
 
 //         TODO() add deep link to set wallpaper
-        val intent = Intent(context, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
         val smallBitmap =
             BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_foreground)
 
-        val pendingIntent: PendingIntent =
-            PendingIntent.getActivity(context, requestCode, intent, 0)
-
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("Test notification")
-            .setContentText("test description")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setLargeIcon(smallBitmap)
-            .setContentIntent(pendingIntent)
 
         when (channel) {
             Channel.NEW_WALLPAPER -> {
-                val largeBitmap =
-                    sharingTools.getBitmap(imageUrl)
-                builder.setStyle(NotificationCompat.BigPictureStyle().bigPicture(largeBitmap))
+                val largeBitmap = imageTools.getBitmapFromRemote(imageUrl)
+                intentDestination = MainActivity::class.java
+                builder
+                    .setStyle(NotificationCompat.BigPictureStyle().bigPicture(largeBitmap))
+                    .setContentTitle("New wallpaper set")
+                    .setContentText("PexWallpapers just set an amazing wallpaper for you")
+                    .setLargeIcon(smallBitmap)
+                    .priority = NotificationCompat.PRIORITY_DEFAULT
             }
             Channel.RECOMMENDATIONS -> {
+                val largeBitmap = imageTools.getBitmapFromRemote(imageUrl)
                 notificationNewWallpaperId = 102
                 requestCode = 2
+                intentDestination = MainActivity::class.java
+                builder
+                    .setStyle(NotificationCompat.BigPictureStyle().bigPicture(largeBitmap))
+                    .setContentTitle("Recommendations")
+                    .setContentText("PexWallpapers have some amazing wallpapers to offer for you")
+                    .setLargeIcon(smallBitmap)
+                    .priority = NotificationCompat.PRIORITY_DEFAULT
             }
             Channel.INFO -> {
                 notificationNewWallpaperId = 103
                 requestCode = 3
-                builder.setStyle(NotificationCompat.BigTextStyle().bigText("asdsd"))
+                intentDestination = MainActivity::class.java
+                builder.setStyle(NotificationCompat.BigTextStyle().bigText(longMessage))
             }
         }
+        val intent = Intent(context, intentDestination).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, requestCode, intent, 0)
+
+        builder.setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         with(NotificationManagerCompat.from(context)) {
             notify(notificationNewWallpaperId, builder.build())
